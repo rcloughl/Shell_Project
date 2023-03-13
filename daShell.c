4e3ee08f68ca4dd3c53ed553ba12f5e27f622057
limpyshrimpy <rcloughl@umw.edu>
2023-03-13 14:49:22 -0400

    started off with breaking up the string

--- daShell.c
+++ daShell.c
@@ -14,17 +14,33 @@ int main(){
 
 
     while (1){
-        printf('snails:~$'); 
+        printf("snails:~$ ");
+        fgets(input, MAX, stdin);
+        input[strlen(input)-1]='\0';
+        char *commands[MAX];
+        char *morepipe;
+        int cmds=0;
+        morepipe=strtok(input,"|"); // This function allows me to break the input up 
+        while (morepipe != 0 ){     // and seperate it on the '|', found here https://linux.die.net/man/3/strtok
+            commands[cmds]=morepipe;
+            morepipe=strtok(0,"|");
+            cmds++;
+        }     
+        
+
+
+
+        
         int p=fork();
+
         if (p < 0) { 
             fprintf(stderr, "fork Failed" ); 
             return 1; 
-
         }
         else if (p > 0) {
-              //execvp
-
+        //execvp();
         }
+
 
 
 
--- makefile
+++ makefile
@@ -1,18 +1,11 @@
-//work in progress
 
 CC = gcc
 CFLAGS = -g -Wall
 
 default: shell
 
-shell: main.o proc.o
-	$(CC) $(CFLAGS) -o shell main.o proc.o
-
-main.o: main.c types.h defs.h
-	$(CC) $(CFLAGS) -c main.c
-
-proc.o: proc.c types.h defs.h proc.h
-	$(CC) $(CFLAGS) -c proc.c
+shell: daShell.c 
+	$(CC) $(CFLAGS) -o shell daShell.c 
 
 clean:
 	rm -f shell *.o
